9d17050913006d949ab5fcf1370a7ed1c6f80eca
Andy Chan <1291473+sockbot@users.noreply.github.com>
2021-12-05 22:28:58 -0800

    solve day 6 part 2

--- 06.py
+++ 06.py
@@ -1,3 +1,5 @@
+import copy
+import collections
 lines = """3,5,1,2,5,4,1,5,1,2,5,5,1,3,1,5,1,3,2,1,5,1,1,1,2,3,1,3,1,2,1,1,5,1,5,4,5,5,3,3,1,5,1,1,5,5,1,3,5,5,3,2,2,4,1,5,3,4,2,5,4,1,2,2,5,1,1,2,4,4,1,3,1,3,1,1,2,2,1,1,5,1,1,4,4,5,5,1,2,1,4,1,1,4,4,3,4,2,2,3,3,2,1,3,3,2,1,1,1,2,1,4,2,2,1,5,5,3,4,5,5,2,5,2,2,5,3,3,1,2,4,2,1,5,1,1,2,3,5,5,1,1,5,5,1,4,5,3,5,2,3,2,4,3,1,4,2,5,1,3,2,1,1,3,4,2,1,1,1,1,2,1,4,3,1,3,1,2,4,1,2,4,3,2,3,5,5,3,3,1,2,3,4,5,2,4,5,1,1,1,4,5,3,5,3,5,1,1,5,1,5,3,1,2,3,4,1,1,4,1,2,4,1,5,4,1,5,4,2,1,5,2,1,3,5,5,4,5,5,1,1,4,1,2,3,5,3,3,1,1,1,4,3,1,1,4,1,5,3,5,1,4,2,5,1,1,4,4,4,2,5,1,2,5,2,1,3,1,5,1,2,1,1,5,2,4,2,1,3,5,5,4,1,1,1,5,5,2,1,1"""
 # lines = """3,4,3,1,2"""
 lines = lines.strip()
@@ -6,8 +8,8 @@ input = [int(num) for num in input]
 
 
 def part1(input):
-    result = input
-    for _ in range(80):
+    result = copy.copy(input)
+    for day in range(80):
         for j in range(len(result)):
             if result[j] == 0:
                 result.append(8)
@@ -17,4 +19,15 @@ def part1(input):
     return len(result)
 
 
+def part2(input):
+    frequency = collections.Counter(input)
+    result = [frequency.get(num, 0) for num in range(9)]
+    for day in range(256):
+        zeroes = result.pop(0)
+        result.append(zeroes)
+        result[6] += zeroes
+    return sum(result)
+
+
 print(part1(input))
+print(part2(input))
